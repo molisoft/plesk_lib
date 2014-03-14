d8d976d1aa7e98849233f584217850327a873e36
Phillipp Röll <phillipp.roell@trafficplex.de>
2014-03-14 18:55:35 +0100

    specify interaction between server and create customer action

--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -1,15 +1,19 @@
 require 'plesk_lib/server'
 
 describe PleskLib::Server do
-  context 'with host, username and password' do
-    subject { PleskLib::Server.new('10.0.0.1', 'admin', 'secret') }
+  subject { PleskLib::Server.new('10.0.0.1', 'admin', 'secret') }
 
-    it { should respond_to(:host) }
-    it { should respond_to(:username) }
-    it { should respond_to(:password) }
+  its(:host)     { should eql '10.0.0.1' }
+  its(:username) { should eql 'admin'    }
+  its(:password) { should eql 'secret'   }
 
-    its(:host)     { should eql '10.0.0.1' }
-    its(:username) { should eql 'admin'    }
-    its(:password) { should eql 'secret'   }
+  describe '#create_customer_account' do
+    it 'uses the PleskLib::Actions::CreateCustomerAccount action' do
+      customer_account = double(:customer_account)
+      PleskLib::Actions::CreateCustomerAccount.should_receive(:new).with(customer_account).and_call_original
+      PleskLib::Actions::CreateCustomerAccount.any_instance.should_receive(:execute_on).with(subject)
+
+      subject.create_customer_account(customer_account)
+    end
   end
 end
